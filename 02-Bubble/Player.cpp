7f6eaa4daf3a30288c85d973a57f2b66af616811
pablo <pabloiniestabdn@gmail.com>
2020-10-30 19:27:25 +0100

    Personaje ojos hecho, full animao

--- 02-Bubble/Player.cpp
+++ 02-Bubble/Player.cpp
@@ -13,32 +13,33 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT, STAND
 };
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/bub.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/pala.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	sprite->setNumberAnimations(5);
 	
-		sprite->setAnimationSpeed(STAND_LEFT, 8);
-		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
+		sprite->setAnimationSpeed(MOVE_UP, 8);
+		sprite->addKeyframe(MOVE_UP, glm::vec2(0.5f, 0.f));
 		
-		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+		sprite->setAnimationSpeed(MOVE_DOWN, 8);
+		sprite->addKeyframe(MOVE_DOWN, glm::vec2(0.25f, 0.f));
 		
 		sprite->setAnimationSpeed(MOVE_LEFT, 8);
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
 		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
+	
 		
 		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
 		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+
+		sprite->setAnimationSpeed(STAND, 8);
+		sprite->addKeyframe(STAND, glm::vec2(0.f, 0.f));
+
 		
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
@@ -57,7 +58,7 @@ void Player::update(int deltaTime)
 	    if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
 		{
 			posPlayer.x += 2;
-			sprite->changeAnimation(STAND_LEFT);
+			sprite->changeAnimation(STAND);
 		}
 		if (Game::instance().getSpecialKey(GLUT_KEY_UP)) //izq + arriba
 		{
@@ -84,7 +85,7 @@ void Player::update(int deltaTime)
 		if(map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
 		{
 			posPlayer.x -= 2;
-			sprite->changeAnimation(STAND_RIGHT);
+			sprite->changeAnimation(STAND);
 		}
 		if (Game::instance().getSpecialKey(GLUT_KEY_UP)) //der + arriba
 		{
@@ -105,6 +106,8 @@ void Player::update(int deltaTime)
 	}
 	else if (Game::instance().getSpecialKey(GLUT_KEY_UP)) //mover arriba
 	{
+		if (sprite->animation() != MOVE_UP)
+			sprite->changeAnimation(MOVE_UP);
 		posPlayer.y -= 2;
 		if (map->collisionMoveUp(posPlayer, glm::ivec2(32, 32)))
 		{
@@ -112,7 +115,9 @@ void Player::update(int deltaTime)
 		}
 	}
 	else if (Game::instance().getSpecialKey(GLUT_KEY_DOWN)) //mover abajo
-	{
+	{	
+		if (sprite->animation() != MOVE_DOWN)
+			sprite->changeAnimation(MOVE_DOWN);
 		posPlayer.y += 2;
 		if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32)))
 		{
@@ -121,10 +126,7 @@ void Player::update(int deltaTime)
 	}
 	else
 	{
-		if(sprite->animation() == MOVE_LEFT)
-			sprite->changeAnimation(STAND_LEFT);
-		else if(sprite->animation() == MOVE_RIGHT)
-			sprite->changeAnimation(STAND_RIGHT);
+		sprite->changeAnimation(STAND);
 	}
 
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
